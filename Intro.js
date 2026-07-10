527103456e9324da30157ee78a7c62e3fb1757d8
TechnoKing <issemwana@gmail.com>
2026-07-10 10:49:43 -0700

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,7 @@
+// Simple Code loging name, age and favorite programming language.
+
+function data(name,age,lang){
+    return "Hello, My name is " + name + ". I am " + age + " Years Old. My Favorite Programming Language is " + lang + ".";
+}
+
+console.log(data('Aaron',25,'Javascript'))
